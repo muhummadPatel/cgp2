0a7610f05a0a1c3444635aebc9c46d4167a50462
Muhummad Patel <muhummad.patel@gmail.com>
2016-03-05 16:54:54 +0200

    Add unit tests to check set operations and voxelising csg trees of various sizes.

--- test/test_scene.cpp
+++ test/test_scene.cpp
@@ -21,9 +21,123 @@ void TestScene::tearDown()
 }
 
 void TestScene::testVoxSetOp_union(){
-    CPPUNIT_ASSERT(1==1);
+    scene->testSetOpScene(SetOp::UNION);
+    scene->voxelise(0.1f);
+
+    int xdim, ydim, zdim;
+    scene->vox.getDim(xdim, ydim, zdim);
+
+    CPPUNIT_ASSERT(scene->vox.get(0, 0, 0) == 0);
+    CPPUNIT_ASSERT(scene->vox.get(xdim, ydim, zdim) == 0);
+
+    CPPUNIT_ASSERT(scene->vox.get(xdim/2, ydim/2, zdim/2) == 1);
+    CPPUNIT_ASSERT(scene->vox.get(xdim/2, ydim/2, (zdim/2)+35) == 1);
+    CPPUNIT_ASSERT(scene->vox.get(xdim/2, ydim/2, (zdim/2)-35) == 1);
+
+    CPPUNIT_ASSERT(scene->vox.get((xdim/2)-65, (ydim/2)-65, zdim/2) == 1);
+    CPPUNIT_ASSERT(scene->vox.get((xdim/2)+65, (ydim/2)+65, zdim/2) == 1);
 
     cerr << "UNION SET OP TEST PASSED" << endl << endl;
+}
+
+void TestScene::testVoxSetOp_difference(){
+    scene->testSetOpScene(SetOp::DIFFERENCE);
+    scene->voxelise(0.1f);
+
+    int xdim, ydim, zdim;
+    scene->vox.getDim(xdim, ydim, zdim);
+
+    CPPUNIT_ASSERT(scene->vox.get(0, 0, 0) == 0);
+    CPPUNIT_ASSERT(scene->vox.get(xdim, ydim, zdim) == 0);
+
+    CPPUNIT_ASSERT(scene->vox.get(xdim/2, ydim/2, zdim/2) == 0);
+    CPPUNIT_ASSERT(scene->vox.get(xdim/2, ydim/2, (zdim/2)+35) == 1);
+    CPPUNIT_ASSERT(scene->vox.get(xdim/2, ydim/2, (zdim/2)-35) ==  1);
+
+    CPPUNIT_ASSERT(scene->vox.get((xdim/2)-65, (ydim/2)-65, zdim/2) == 0);
+    CPPUNIT_ASSERT(scene->vox.get((xdim/2)+65, (ydim/2)+65, zdim/2) == 0);
+
+    cerr << "DIFFERENCE SET OP TEST PASSED" << endl << endl;
+}
+
+void TestScene::testVoxSetOp_intersection(){
+    scene->testSetOpScene(SetOp::INTERSECTION);
+    scene->voxelise(0.1f);
+
+    int xdim, ydim, zdim;
+    scene->vox.getDim(xdim, ydim, zdim);
+
+    CPPUNIT_ASSERT(scene->vox.get(0, 0, 0) == 0);
+    CPPUNIT_ASSERT(scene->vox.get(xdim, ydim, zdim) == 0);
+
+    CPPUNIT_ASSERT(scene->vox.get(xdim/2, ydim/2, zdim/2) == 1);
+    CPPUNIT_ASSERT(scene->vox.get(xdim/2, ydim/2, (zdim/2)+35) == 0);
+    CPPUNIT_ASSERT(scene->vox.get(xdim/2, ydim/2, (zdim/2)-35) ==  0);
+
+    CPPUNIT_ASSERT(scene->vox.get((xdim/2)-65, (ydim/2)-65, zdim/2) == 0);
+    CPPUNIT_ASSERT(scene->vox.get((xdim/2)+65, (ydim/2)+65, zdim/2) == 0);
+
+    cerr << "INTERSECTION SET OP TEST PASSED" << endl << endl;
+}
+
+void TestScene::testVoxelise1Shape(){
+    scene->testVoxeliseScene(1);
+    scene->voxelise(0.1f);
+
+    int xdim, ydim, zdim;
+    scene->vox.getDim(xdim, ydim, zdim);
+
+    CPPUNIT_ASSERT(scene->vox.get(0, 0, 0) == 0);
+    CPPUNIT_ASSERT(scene->vox.get(xdim, ydim, zdim) == 0);
+
+    CPPUNIT_ASSERT(scene->vox.get(xdim/2, ydim/2, zdim/2) == 1);
+    CPPUNIT_ASSERT(scene->vox.get(xdim/2, ydim/2, (zdim/2)+35) == 1);
+    CPPUNIT_ASSERT(scene->vox.get(xdim/2, ydim/2, (zdim/2)-35) ==  1);
+
+    cerr << "VOXELISE 1 SHAPE CSG TREE TEST PASSED" << endl << endl;
+}
+
+void TestScene::testVoxelise2Shape(){
+    scene->testVoxeliseScene(2, SetOp::UNION);
+    scene->voxelise(0.1f);
+
+    int xdim, ydim, zdim;
+    scene->vox.getDim(xdim, ydim, zdim);
+
+    CPPUNIT_ASSERT(scene->vox.get(0, 0, 0) == 0);
+    CPPUNIT_ASSERT(scene->vox.get(xdim, ydim, zdim) == 0);
+
+    CPPUNIT_ASSERT(scene->vox.get(xdim/2, ydim/2, zdim/2) == 1);
+    CPPUNIT_ASSERT(scene->vox.get(xdim/2, ydim/2, (zdim/2)+35) == 1);
+    CPPUNIT_ASSERT(scene->vox.get(xdim/2, ydim/2, (zdim/2)-35) ==  1);
+
+    CPPUNIT_ASSERT(scene->vox.get((xdim/2)-65, ydim/2, zdim/2) == 1);
+    CPPUNIT_ASSERT(scene->vox.get((xdim/2)+65, ydim/2, zdim/2) == 1);
+
+    cerr << "VOXELISE 2 SHAPE CSG TREE TEST PASSED" << endl << endl;
+}
+
+void TestScene::testVoxelise3Shape(){
+    scene->testVoxeliseScene(3, SetOp::UNION, SetOp::DIFFERENCE);
+    scene->voxelise(0.1f);
+
+    int xdim, ydim, zdim;
+    scene->vox.getDim(xdim, ydim, zdim);
+
+    CPPUNIT_ASSERT(scene->vox.get(0, 0, 0) == 0);
+    CPPUNIT_ASSERT(scene->vox.get(xdim, ydim, zdim) == 0);
+
+    CPPUNIT_ASSERT(scene->vox.get(xdim/2, ydim/2, zdim/2) == 0);
+    CPPUNIT_ASSERT(scene->vox.get(xdim/2, ydim/2, (zdim/2)+35) == 1);
+    CPPUNIT_ASSERT(scene->vox.get(xdim/2, ydim/2, (zdim/2)-35) ==  1);
+
+    CPPUNIT_ASSERT(scene->vox.get((xdim/2)-65, ydim/2, zdim/2) == 1);
+    CPPUNIT_ASSERT(scene->vox.get((xdim/2)+65, ydim/2, zdim/2) == 1);
+
+    CPPUNIT_ASSERT(scene->vox.get(xdim/2, (ydim/2)-65, zdim/2) == 0);
+    CPPUNIT_ASSERT(scene->vox.get(xdim/2, (ydim/2)+65, zdim/2) == 0);
+
+    cerr << "VOXELISE 3 SHAPE CSG TREE TEST PASSED" << endl << endl;
 }
 
 //#if 0 /* Disabled since it crashes the whole test suite */
--- test/test_scene.h
+++ test/test_scene.h
@@ -4,13 +4,22 @@
 
 #include <string>
 #include <cppunit/extensions/HelperMacros.h>
+
+// So that we can inspect the private members of Scene
+#define private public
 #include "tesselate/csg.h"
+#define private private
 
 /// Test code for @ref Scene
 class TestScene : public CppUnit::TestFixture
 {
     CPPUNIT_TEST_SUITE(TestScene);
     CPPUNIT_TEST(testVoxSetOp_union);
+    CPPUNIT_TEST(testVoxSetOp_difference);
+    CPPUNIT_TEST(testVoxSetOp_intersection);
+    CPPUNIT_TEST(testVoxelise1Shape);
+    CPPUNIT_TEST(testVoxelise2Shape);
+    CPPUNIT_TEST(testVoxelise3Shape);
     CPPUNIT_TEST_SUITE_END();
 
 private:
@@ -28,6 +37,31 @@ public:
      * Test that the UNION set operation produces the expected results
      */
     void testVoxSetOp_union();
+
+    /**
+     * Test that the DIFFERENCE set operation produces the expected results
+     */
+    void testVoxSetOp_difference();
+
+    /**
+     * Test that the INTERSECTION set operation produces the expected results
+     */
+    void testVoxSetOp_intersection();
+
+    /**
+     * Test that a csg tree with 1 shape is properly voxelised
+     */
+    void testVoxelise1Shape();
+
+    /**
+     * Test that a csg tree with 2 shapes is properly voxelised
+     */
+    void testVoxelise2Shape();
+
+    /**
+     * Test that a csg tree with 3 shapes is properly voxelised
+     */
+    void testVoxelise3Shape();
 };
 
 #endif /* !TILER_TEST_SCENE_H */
